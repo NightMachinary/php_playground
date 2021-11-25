abf50548d7aef121f2131abdd879f8edb7251155
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-11-25 11:27:09 +0330

    modified: a.php

--- a.php
+++ a.php
@@ -1,1 +1,11 @@
-Hello templating, <?php echo 34?>!
+<?php $name = "VYVA"?>
+Hello templating, <?php echo $name?>!
+
+<?php
+
+$animals_list = array("Lion","Wolf","Dog","Leopard","Tiger");
+
+foreach($animals_list as $array_values){ ?>
+    In the 🌲️, <?=$array_values?>s sleep tonight ...
+
+<?php }?>
